dde281ebc8860209d342471ebf78f6d980f6c7a9
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-25 16:40:18 -0500

    planning and organizing

--- assets/script.js
+++ assets/script.js
@@ -48,30 +48,33 @@ DOM TRAVERSAL(Maybe?)
 
 //Create functions then call it within the DOM.
 $(document).ready(function () {
-
+  // a function?
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
-  
   // local storage. HINT: What does `this` reference in the click listener
   //---->more research on the 'this' keyword.
   // function? How can DOM traversal be used to get the "hour-x" id of the
-  
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  //-------------------------------------------------------
+  // A function
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  //--------------------------------------------------------------
+  // A function
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  //------------------------------------------------------
+  // a function: TODO: Add code to display the current date in the header of the page.
 
   //WHEN FINISH CALL ALL FUNCTIONS HERE
+  // saveBtn();
+  // displayDate();
+  // getUserData();
 });
 
 function saveBtn () {
@@ -88,6 +91,6 @@ function displayDate () {
  var presentDate = dayjs().
 }
 
-function userData () {
+function getUserData () {
 
 }
