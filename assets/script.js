ece9ced8e4d45bfdc6b49a4f8767d4660697a525
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-25 16:45:47 -0500

    organizing

--- assets/script.js
+++ assets/script.js
@@ -70,13 +70,23 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   //------------------------------------------------------
   // a function: TODO: Add code to display the current date in the header of the page.
-
+  //---------------------------------------
   //WHEN FINISH CALL ALL FUNCTIONS HERE
   // saveBtn();
   // displayDate();
   // getUserData();
 });
+/*
+  **HTML CONTAINER**
+   <div id="hour-9" class="row time-block past">
+        <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+          <i class="fas fa-save" aria-hidden="true"></i>
+        </button>
+      </div>
 
+*/
 function saveBtn () {
   $(".saveBtn").on("click", function() {
     var timeBlock = $(this).
