b4f4700ef9e44e3e3c3d33a308e4c63f13dcbb2a
Arthur Bridges <killor@Arthurs-MBP.lan>
2023-09-24 23:28:54 -0500

    planning

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Scheduling-website
-Create you schedule
+#Scheduler
+
+##Description
+
+A webpage that allows you to organize your week 
+
+##Installation
+
+N/A
+
+##Usage
+Deployed website is here:
+
+N/A
+
+##License 
+
+Credit to the author of this repo.
---
+++ assets/script.js
@@ -0,0 +1,30 @@
+/*
+1) create array and objects Days of the week and times.
+
+2) create for loop to go through each iteration of the day displaying the times.
+
+3) accessibility with upcoming, past and current times.
+
+4) color code those times.
+*/
+
+/*       **SYNTAX FOR OBJECTS USING DAYJS API**
+
+
+dayjs.extend(objectSupport)
+dayjs({ hour:15, minute:10 });
+dayjs.utc({ y:2010, M:3, d:5, h:15, m:10, s:3, ms: 123});
+dayjs({ year :2010, month :3, day :5, hour :15, minute :10, second :3, millisecond :123});
+dayjs({ years:2010, months:3, date:5, hours:15, minutes:10, seconds:3, milliseconds:123});
+
+ */
+
+dayjs.extent(objectSupport);
+
+var day = {
+
+}
+
+var time = {
+
+}
